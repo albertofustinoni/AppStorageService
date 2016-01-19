e2bfbbb3819631332e6d6380ae1f47a99a1e218a
Alberto Fustinoni <alberto@aftnet.net>
2016-01-20 01:52:55 +0900

    Fixes

--- AppStorageService.Universal.Test/Properties/AssemblyInfo.cs
+++ AppStorageService.Universal.Test/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AppStorageService Universal Test")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyMetadata("TargetPlatform","UAP")]
