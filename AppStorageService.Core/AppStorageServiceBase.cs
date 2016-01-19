fe461da096a7f499ee0eadd791206e0b441176c8
Alberto Fustinoni <alberto-fustinoni@outlook.com>
2016-01-19 13:13:33 +0900

    Logic

---
+++ AppStorageService.Core/AppStorageServiceBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace AppStorageService.Core
+{
+    internal abstract class AppStorageServiceBase<TData> : IAppStorageService<TData> where TData : class
+    {
+        public readonly string FileName;
+        public bool OperationInProgress { get; private set; }
+
+        public AppStorageServiceBase(string fileName)
+        {
+            FileName = fileName;
+            OperationInProgress = false;
+        }
+
+        public abstract Task SaveDataAsync(TData data);
+
+        public abstract Task<TData> LoadDataAsync();
+    }
+}
