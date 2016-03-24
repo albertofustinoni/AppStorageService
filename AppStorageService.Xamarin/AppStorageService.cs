755b8265ae0dccb1f1936fa44c522795e290375f
Alberto Fustinoni <alberto.fustinoni@rakuten.com>
2016-03-24 16:44:36 +0900

    Added xamarin project

---
+++ AppStorageService.Xamarin/AppStorageService.cs
@@ -0,0 +1,65 @@
+﻿using AppStorageService.Core;
+using PCLStorage;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace AppStorageService.Xamarin
+{
+    public class AppStorageService<TData> : AppStorageServiceBase<TData> where TData : class
+    {
+        public AppStorageService(string fileName) : base(fileName) { }
+
+        protected override async Task<TData> LoadDataAsyncLogic()
+        {
+            var file = await GetFile(FileName, false);
+            if (file == null)
+            {
+                return null;
+            }
+
+            using (var stream = await file.OpenAsync(FileAccess.Read))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(TData));
+                var output = (TData)serializer.ReadObject(stream);
+                return output;
+            }
+        }
+
+        protected override async Task SaveDataAsyncLogic(TData data)
+        {
+            var file = await GetFile(FileName, true);
+
+            using (var stream = await file.OpenAsync(FileAccess.ReadAndWrite))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(TData));
+                serializer.WriteObject(stream, data);
+            }
+        }
+
+        protected override async Task DeleteDataAsyncLogic()
+        {
+            var file = await GetFile(FileName, false);
+            if (file != null)
+            {
+                await file.DeleteAsync();
+            }
+        }
+
+        private static async Task<IFile> GetFile(string fileName, bool createIfNotExists)
+        {
+            IFile output = null;
+            var folder = FileSystem.Current.LocalStorage;
+            var existResult = await folder.CheckExistsAsync(fileName);
+            if (existResult == ExistenceCheckResult.FileExists)
+            {
+                output = await folder.GetFileAsync(fileName);
+            }
+            else if (createIfNotExists)
+            {
+                output = await folder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ AppStorageService.Xamarin/AppStorageServiceCached.cs
@@ -0,0 +1,14 @@
+﻿using AppStorageService.Core;
+
+namespace AppStorageService.Xamarin
+{
+    public abstract class AppStorageServiceCached<TData> : AppStorageServiceCachedBase<AppStorageService<TData>, TData> where TData : class
+    {
+        protected override AppStorageService<TData> CreateBackingServiceInstance(string fileName)
+        {
+            return new AppStorageService<TData>(fileName);
+        }
+
+        public AppStorageServiceCached(string fileName) : base(fileName) { }
+    }
+}
